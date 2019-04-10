6af7b6b52d4c2edd8d84bfaf7e3faa29c9c48846
Maurice Frank <maurice.frank@posteo.de>
2019-04-10 12:51:49 +0200

    Added newest changes

--- simulate.py
+++ simulate.py
@@ -31,6 +31,7 @@ class Quantity(object):
     name: str
     magnitude: MClass
     derivative: DClass
+    exogenous: bool
 
 
 @dataclass
@@ -55,11 +56,18 @@ class IDependency(object):
 
 
 quantities = [
-    Quantity('Inflow', UnboundedMagnitude,  Derivative),
-    Quantity('Volume', BoundedMagnitude,  Derivative),
-    Quantity('Outflow', BoundedMagnitude,  Derivative)
+    Quantity('Inflow', UnboundedMagnitude,  Derivative, True),
+    Quantity('Volume', BoundedMagnitude,  Derivative, False),
+    Quantity('Outflow', BoundedMagnitude,  Derivative, False)
 ]
 
+quantities = [
+    Quantity('Inflow', UnboundedMagnitude,  Derivative, True),
+    Quantity('Volume', BoundedMagnitude,  Derivative, False),
+    Quantity('Outflow', BoundedMagnitude,  Derivative, False),
+    Quantity('Height', BoundedMagnitude,  Derivative, False),
+    Quantity('Pressure', BoundedMagnitude,  Derivative, False),
+]
 
 dependencies = [
     VCDependency('Volume', 'Outflow', 'MAX', 'MAX'),
@@ -67,6 +75,18 @@ dependencies = [
     PDependency('Volume', 'Outflow'),
     IDependency('Inflow', 'Volume', 'POS'),
     IDependency('Outflow', 'Volume', 'NEG')
+]
+
+dependencies = [
+    VCDependency('Volume', 'Outflow', 'MAX', 'MAX'),
+    VCDependency('Volume', 'Outflow', 'ZERO', 'ZERO'),
+    VCDependency('Pressure', 'Outflow', 'MAX', 'MAX'),
+    VCDependency('Pressure', 'Outflow', 'ZERO', 'ZERO'),
+    IDependency('Inflow', 'Volume', 'POS'),
+    IDependency('Outflow', 'Volume', 'NEG'),
+    PDependency('Pressure', 'Outflow'),
+    PDependency('Volume', 'Height'),
+    PDependency('Height', 'Pressure')
 ]
 
 
@@ -94,8 +114,7 @@ def node_filter(dependencies):
         return True
     return _filter
 
-
-def edge_filter(idependencies):
+def edge_filter(dependencies):
     def _filter(pair):
         old, new = pair
 
@@ -125,7 +144,7 @@ def edge_filter(idependencies):
 
         # Filter out false influences
         influenceSums = {}
-        for idependency in idependencies:
+        for idependency in filter(lambda x:x.__class__==IDependency, dependencies):
             if old[qIdx[idependency.start]][0] == 'ZERO':
                 continue
             endIdx = qIdx[idependency.end]
@@ -140,13 +159,39 @@ def edge_filter(idependencies):
                 if new[endIdx][1] != sign:
                     return False
 
-        # for i in range(len(old)):
-        #     if i in influenceSums:
-        #         continue
-        #     if old[i][1] != new[i][1]:
-        #         if not ((old[i][1] == 'POS' and new[i][1] == 'ZERO' and new[i][0] == 'MAX') or \
-        #                 (old[i][1] == 'NEG' and new[i][1] == 'ZERO' and new[i][0] == 'ZERO')):
-        #             return False
+        for  i in range(len(old)):
+            oldMag, oldDrv = old[i]
+            newMag, newDrv = new[i]
+            if quantities[i].exogenous or oldDrv == newDrv:
+                continue
+            if oldDrv == 'POS' and newDrv == 'ZERO' and newMag == 'MAX':
+                continue
+            if oldDrv == 'NEG' and newDrv == 'ZERO' and newMag == 'ZERO':
+                continue
+            if i in influenceSums:
+                if influenceSums[i] is not None or influenceSums[i] == newDrv:
+                    edge_labels[pair] = True
+                    continue
+            cont = False
+            for dependency in filter(lambda x:x.__class__==PDependency, dependencies):
+                if quantities[i].name == dependency.left_name and new[qIdx[dependency.right_name]][1] == newDrv:
+                    cont = True
+                if quantities[i].name == dependency.right_name and new[qIdx[dependency.left_name]][1] == newDrv:
+                    cont = True
+            if cont:
+                continue
+            return False
+
+        for dependency in filter(lambda x:x.__class__==PDependency, dependencies):
+            i_left = qIdx[dependency.left_name]
+            i_right = qIdx[dependency.right_name]
+            if old[i_left][1] == new[i_left][1] and old[i_left][1] == new[i_right][1]:
+                continue
+            if quantities[i_left].exogenous or quantities[i_right].exogenous:
+                continue
+            if i_left in influenceSums or i_right in influenceSums:
+                continue
+            return False
         return True
     return _filter
 
@@ -154,6 +199,20 @@ def edge_filter(idependencies):
 def repr_state(s):
     rs = {'MAX': 'max', 'POS': '+', 'NEG': '-', 'ZERO': '0'}
     return 'In[{},{}]\nVol[{},{}]\nOut[{},{}]'.format(rs[s[0][0]], rs[s[0][1]], rs[s[1][0]], rs[s[1][1]], rs[s[2][0]], rs[s[2][1]])
+
+
+qIdx = {q.name:i for i,q in enumerate(quantities)}
+
+quantity_states = [product(q.magnitude._member_names_,q.derivative._member_names_) for q in quantities]
+filtered_quantity_states = [filter(quantity_filter, qs) for qs in quantity_states]
+
+node_states = list(product(*filtered_quantity_states))
+filtered_node_states = list(filter(node_filter(dependencies), node_states))
+
+
+edge_labels = {}
+edge_states = list(product(filtered_node_states, filtered_node_states))
+filtered_edge_states = list(filter(edge_filter(dependencies), edge_states))
 
 
 def save_graph(nodes, edges):
@@ -165,24 +224,29 @@ def save_graph(nodes, edges):
         normalize=True,
         smoothing='avg_dist'
     )
-    G.add_edges_from(filtered_edge_states)
+    G.add_edges_from(edges)
+    for edge,label in edge_labels.items():
+        if G.has_edge(edge[0], edge[1]):
+            G.get_edge(edge[0], edge[1]).attr['label'] = label
+    for edge in edges:
+        old,new = edge
+        labels = []
+        for i in range(len(old)):
+            if new[i][0] != old[i][0] and old[i][1] != 'ZERO':
+                labels.append('∂')
+            if quantities[i].exogenous and old[i][1] != new[i][1]:
+                labels.append('exo')
+        if edge in edge_labels:
+            labels.append('I')
+        G.get_edge(edge[0], edge[1]).attr['label'] = ','.join(labels)
     for node in nodes:
         if G.has_node(node):
             G.get_node(node).attr['label'] = repr_state(node)
-    G.draw('test.pdf', prog='circo')
-
-
-idependencies =  list(filter(lambda x: x.__class__ == IDependency, dependencies))
-qIdx = {q.name:i for i,q in enumerate(quantities)}
-
-quantity_states = [product(q.magnitude._member_names_,q.derivative._member_names_) for q in quantities]
-filtered_quantity_states = [filter(quantity_filter, qs) for qs in quantity_states]
-
-node_states = list(product(*filtered_quantity_states))
-filtered_node_states = list(filter(node_filter(dependencies), node_states))
-
-edge_states = list(product(filtered_node_states, filtered_node_states))
-filtered_edge_states = list(filter(edge_filter(idependencies), edge_states))
+            for dependency in filter(lambda x:x.__class__==VCDependency, dependencies):
+                    if node[qIdx[dependency.left_name]][0] == dependency.left_mag:
+                        G.get_node(node).attr['style'] = 'dashed'
+                        G.get_node(node).attr['color'] = 'blue'
+    G.draw('test.pdf', prog='dot')
 
 save_graph(filtered_node_states, filtered_edge_states)
 
